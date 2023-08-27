c2de295fc629f7d18cecc96c63ed174073af8052
Claudio <71095411+rojas-claudio@users.noreply.github.com>
2023-08-27 16:34:07 -0700

    Fixed GitHub URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,14 @@
 */
 
 import Link from "next/link"
+
 export default function Index() {
   return (
     <div className="flex h-screen flex-col justify-center items-center">
       <title>claudio rojas</title>
       <div className="flex flex-col gap-1 text-2xl">
         <Link className="underline-global" href="/photography">Photography</Link>
-        <a href="https://github.com/itsthered1" className="underline-global" target="_blank">Programming</a>
+        <a href="https://github.com/rojas-claudio" className="underline-global" target="_blank">Programming</a>
         <a href="https://albumoftheyear.org/user/mrclaudio" className="underline-global" target="_blank">Music Reviews</a>
         <Link href="/about">About</Link>
       </div>
